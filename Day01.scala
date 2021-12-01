3c94f8afa99b1344cd66cbade70c8d5bb739acbc
Radek Stibora <rstibora@gmail.com>
2021-12-01 21:06:43 +0100

    Add second star solution for day 1

--- Day01.scala
+++ Day01.scala
@@ -1,15 +1,25 @@
 import scala.io.Source
 
-@main def solution() = 
-    val depthMeasurements = Source.fromFile("./inputs/day01").getLines().toArray
-
+def coundDepthIncreases(depthMeasurements: Array[Int]): Int = 
     var depthIncreasesCount = 0
-    var previousDepth = depthMeasurements(0).toInt
+    var previousDepth = depthMeasurements(0)
     for
         depthMeasurement <- depthMeasurements
     do
         if depthMeasurement.toInt > previousDepth then 
             depthIncreasesCount += 1
-        previousDepth = depthMeasurement.toInt
+        previousDepth = depthMeasurement
+    return depthIncreasesCount
 
-    println(depthIncreasesCount)
+
+def filter(rawDepthMeasurements: Array[Int], windowSize: Int): Array[Int] = 
+    return rawDepthMeasurements.sliding(windowSize).map(window => window.sum).toArray
+
+def firstStar() = 
+    val depthMeasurements = Source.fromFile("./inputs/day01").getLines().map(line => line.toInt).toArray
+    println(coundDepthIncreases(depthMeasurements))
+
+@main def secondStar() =
+    val depthMeasurements = Source.fromFile("./inputs/day01").getLines().map(line => line.toInt).toArray
+    val filteredMeasurements = filter(depthMeasurements, windowSize = 3)
+    println(coundDepthIncreases(filteredMeasurements))
