402940146b9d188020cb6531f8c4ac15a4e75b63
Radek Stibora <rstibora@gmail.com>
2021-12-08 18:32:07 +0100

    Update day 2 with changes based on 'Scala Advent of Code'

--- Day02.scala
+++ Day02.scala
@@ -6,45 +6,37 @@ enum Instruction:
     case Up(amount: Int)
     case Down(amount: Int)
 
-def parseInstruction(instruction: String): Instruction =
-    import Instruction.*
-
-    val amount = instruction.split(" ")(1).toInt
-    return instruction.split(" ")(0) match
-        case "forward" => return Forward(amount)
-        case "up" => return Up(amount)
-        case "down" => return Down(amount)
+object Instruction:
+    def from(instructionLine: String): Instruction =
+        instructionLine match
+            case s"forward $x" if x.toIntOption.isDefined => Forward(x.toInt)
+            case s"up $x" if x.toIntOption.isDefined => Up(x.toInt)
+            case s"down $x" if x.toIntOption.isDefined => Down(x.toInt)
+            case _ => throw new Exception(s"$instructionLine is not a valid command.")
 
 def firstStar() =
-    import Instruction.*
-
     var xPosition = 0
     var depth = 0
     for
-        instruction <- Source.fromFile("./inputs/day02").getLines.map(parseInstruction)
+        instruction <- Source.fromFile("./inputs/day02").getLines.map(Instruction.from)
     do
-        instruction match {
-            case Forward(amount) => xPosition += amount
-            case Up(amount) => depth -= amount
-            case Down(amount) => depth += amount
-        }
-
+        instruction match
+            case Instruction.Forward(amount) => xPosition += amount
+            case Instruction.Up(amount) => depth -= amount
+            case Instruction.Down(amount) => depth += amount
     println(xPosition * depth)
 
 @main def secondStar() =
-    import Instruction.*
-
     var xPosition = 0
     var depth = 0
     var aim = 0
     for
-        instruction <- Source.fromFile("./inputs/day02").getLines.map(parseInstruction)
+        instruction <- Source.fromFile("./inputs/day02").getLines.map(Instruction.from)
     do
-        instruction match {
-            case Forward(amount) => 
+        instruction match
+            case Instruction.Forward(amount) => 
                 xPosition += amount
                 depth += amount * aim
-            case Up(amount) => aim -= amount
-            case Down(amount) => aim += amount
-        }
+            case Instruction.Up(amount) => aim -= amount
+            case Instruction.Down(amount) => aim += amount
     println(xPosition * depth)
