c58d1c8846edf7f1e807776cf36f0c1ea154c510
Radek Stibora <rstibora@gmail.com>
2021-12-08 18:09:00 +0100

    Add second star solution for day 3

--- Day03.scala
+++ Day03.scala
@@ -2,16 +2,41 @@ import scala.io.Source
 import scala.math.pow
 
 def parseDiagnosticLine(diagnosticLine: String): Vector[Int] =
-    return diagnosticLine.map(digit => if digit == '1' then 1 else 0).toVector
+    diagnosticLine.map(digit => if digit == '1' then 1 else 0).toVector
 
 def toDecimal(diagnosticLineBits: Vector[Int]): Int =
     diagnosticLineBits.reverse.zipWithIndex.map((bit, index) => bit * pow(2, index).intValue).sum
 
-@main def firstStar() =
-    val diagnosticReport = Source.fromFile("./inputs/day03").getLines.map(parseDiagnosticLine).toVector
-    val diagnosticReportLenght = diagnosticReport.length
+def findMostCommonBits(parsedDiagnosticLines: Vector[Vector[Int]]): Vector[Option[Int]] =
+    val summedBits = parsedDiagnosticLines.reduce((diagnosticLineA, diagnosticLineB) => diagnosticLineA.zip(diagnosticLineB).map((a: Int, b: Int) => a + b))
+    summedBits.map(diagnostiBitsCount => if diagnostiBitsCount > parsedDiagnosticLines.length - diagnostiBitsCount then Some(1)
+                                         else if diagnostiBitsCount == parsedDiagnosticLines.length - diagnostiBitsCount then Option.empty
+                                         else Some(0))
 
-    val summedBits = diagnosticReport.reduce((diagnosticLineA, diagnosticLineB) => diagnosticLineA.zip(diagnosticLineB).map((a: Int, b: Int) => a + b))
-    val mostCommonBits = summedBits.map(diagnostiBitsCount => if diagnostiBitsCount > diagnosticReportLenght / 2 then 1 else 0)
-    val leastCommonBits = mostCommonBits.map(bit => if bit == 1 then 0 else 1).toVector
+def findLeastCommonBits(parsedDiagnosticLines: Vector[Vector[Int]]): Vector[Option[Int]] =
+    findMostCommonBits(parsedDiagnosticLines).map(_.map((bit) => if bit == 1 then 0 else 1))
+
+def filterAccordingNthBit(diagnosticLines: Vector[Vector[Int]], bitPosition: Int, criterium: (Vector[Vector[Int]]) => Vector[Option[Int]], preferredBitValue: Int): Vector[Vector[Int]] =
+    if diagnosticLines.length == 1 then return diagnosticLines
+
+    val processedLines = criterium(diagnosticLines).map(_.getOrElse(preferredBitValue))
+    diagnosticLines.filter(diagnosticLine => diagnosticLine(bitPosition) == processedLines(bitPosition))
+
+def firstStar() =
+    val diagnosticReport = Source.fromFile("./inputs/day03").getLines.map(parseDiagnosticLine).toVector
+    val mostCommonBits = findMostCommonBits(diagnosticReport).map(_.getOrElse(1))
+    val leastCommonBits = findLeastCommonBits(diagnosticReport).map(_.getOrElse(1))
     println(toDecimal(mostCommonBits) * toDecimal(leastCommonBits))
+
+@main def secondStar() =
+    val diagnosticReport = Source.fromFile("./inputs/day03").getLines.map(parseDiagnosticLine).toVector
+    var filteredReport = diagnosticReport
+    for bitPosition <- 0 until filteredReport(0).length do
+        filteredReport = filterAccordingNthBit(filteredReport, bitPosition, findMostCommonBits, preferredBitValue = 1)
+    val oxygenGeneratorRating = toDecimal(filteredReport(0))
+
+    filteredReport = diagnosticReport
+    for bitPosition <- 0 until filteredReport(0).length do
+        filteredReport = filterAccordingNthBit(filteredReport, bitPosition, findLeastCommonBits, preferredBitValue = 0)
+    val co2ScrubberRating =  toDecimal(filteredReport(0))
+    println(oxygenGeneratorRating * co2ScrubberRating)
