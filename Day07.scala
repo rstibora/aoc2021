6df59650b7c009166dbbb62d1be123dfab3a7ca5
Radek Stibora <rstibora@gmail.com>
2021-12-14 20:57:13 +0100

    Add second star solution for day 7

--- Day07.scala
+++ Day07.scala
@@ -1,11 +1,26 @@
 import scala.io.Source
 
 
-@main def firstStar() =
+def firstStar() =
     val positions = Source.fromFile("./inputs/day07").getLines.next.split(",").map(_.toInt)
     val minPosition = positions.min
     val maxPosition = positions.max
+
     val positionPrices = (minPosition to maxPosition).map(
         possiblePosition => (possiblePosition,
                              positions.map(position => (position - possiblePosition).abs).sum))
+
+    println(positionPrices.minBy((position, price) => price)._2)
+
+@main def secondStar() =
+    val positions = Source.fromFile("./inputs/day07").getLines.next.split(",").map(_.toInt)
+    val minPosition = positions.min
+    val maxPosition = positions.max
+
+    val positionPrices = (minPosition to maxPosition).map(
+        possiblePosition => (possiblePosition,
+                             positions.map(position => 
+                                 val n = (position - possiblePosition).abs
+                                 (n * (n + 1)) / 2
+                             ).sum))
     println(positionPrices.minBy((position, price) => price)._2)
